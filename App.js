644abcd456b8867d71176cb53b10d7c11babed39
tommynguyen <tntt_tom@me.com>
2020-03-23 20:25:03 -0500

    UI Layout for Onboard High School Screen

--- App.js
+++ App.js
@@ -38,7 +38,7 @@ const Stack = createStackNavigator();
 function App()  {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="OnboardEducation">
+      <Stack.Navigator initialRouteName="OnboardHighSchool">
         <Stack.Screen name ="Welcome" component ={WelcomeScreen} options={{headerShown: false}} />
         <Stack.Screen name ="Register" component={RegisterScreen} options={{headerShown: false}} />
         <Stack.Screen name ="Login" component={LoginScreen} />
