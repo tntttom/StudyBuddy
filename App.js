078d54c836740565cfadabec266546c927def13d
tommynguyen <tntt_tom@me.com>
2020-03-30 11:07:07 -0500

    Home Screen WIP

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ const Stack = createStackNavigator();
 function App()  {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Profile">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name ="Welcome" component ={WelcomeScreen} options={{headerShown: false}} />
         <Stack.Screen name ="Register" component={RegisterScreen} options={{headerShown: false}} />
         <Stack.Screen name ="Login" component={LoginScreen} options={{headerShown: false}}/>
@@ -45,8 +45,8 @@ function App()  {
         <Stack.Screen name ="OnboardEducation" component={OnboardEducationScreen} options={{headerShown: false}}/>
         <Stack.Screen name ="OnboardHighSchool" component={OnboardHighSchoolScreen} options={{headerShown: false}}/>
         <Stack.Screen name ="OnboardCollege" component={OnboardCollegeScreen} options={{headerShown: false}}/>
-        <Stack.Screen name ="Profile" component={ProfileScreen} options={{title:'study', headerTitleStyle: {fontFamily: 'Montserrat-Medium', fontSize: 32}, headerShown:false}}/>
-        <Stack.Screen name ="Home" component={HomeScreen} />
+        <Stack.Screen name ="Profile" component={ProfileScreen} options={{headerShown:false}}/>
+        <Stack.Screen name ="Home" component={HomeScreen} options={{title:'study', headerTitleStyle: {fontFamily: 'Montserrat-Medium', fontSize: 32}, }}/>
         <Stack.Screen name ="StudyDetails" component={StudyDetailsScreen} />
 
         
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,16 +1,34 @@
-
 import * as React from 'react';
-import {View, Text, AsyncStorage} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, TextInput, Dimensions, AsyncStorage} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default class HomeScreen extends React.Component{
     render() {
         return (
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>study</Text>
+            <View style={styles.container}>
+
+                <ScrollView
+                showsVerticalScrollIndicator={false}>
+                    <View style={styles.cardContainer}>
+                        <Text style={styles.cardHeaderText}> Card</Text>
+                    </View>
+
+                    <View style={styles.cardContainer}>
+                        <Text style={styles.cardHeaderText}> Card</Text>
+                    </View>
+
+                    <View style={styles.cardContainer}>
+                        <Text style={styles.cardHeaderText}> Card</Text>
+                    </View>
+
+                </ScrollView>
+                
             </View>
         );
 
     }
+
 
     // Save database graphs into async storage as 'graphs'
     saveData = async (graphs) => {
@@ -37,3 +55,35 @@ export default class HomeScreen extends React.Component{
     }
     
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'flex-start', 
+        backgroundColor: 'white'
+        
+    },
+
+    cardContainer: {
+        flex: 0.2,
+        backgroundColor: 'blue',
+        marginBottom: 15,
+        marginTop: 15,
+        height: 300,
+        width: 300,
+        borderRadius: 18,
+        alignItems:'center',
+    },
+
+    cardHeaderText: {
+        fontFamily: 'Montserrat-Medium',
+        fontSize: 36,
+        textAlign:'center',
+
+    },
+
+
+
+
+})
